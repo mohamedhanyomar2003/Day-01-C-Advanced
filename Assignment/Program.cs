e696b690d48aa40146d6f1105202320f02584cd2
compuremo <compuremo@DESKTOP-BIMGJ29>
2025-08-24 22:46:14 +0300

    Q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,7 +1,22 @@
-﻿namespace Assignment
+﻿using System.Formats.Tar;
+
+namespace Assignment
 {
     internal class Program
     {
+        static void Reverse<T>(ref T []arr)
+        {
+            if (arr is not null)
+           for (int i=0;i<arr.Length; ++i)
+                {
+                    for (int j = 0; j < arr.Length - 1 - i; ++j)
+                    {
+                        T temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+        }
         static void Main(string[] args)
         {
             #region Q1
@@ -19,6 +34,26 @@
             //Range<int> r1 = new Range<int>(1, 8);
             //Console.WriteLine(r1.IsInRange(0));
             //Console.WriteLine(r1.Length());
+            #endregion
+
+            #region Q2
+            ////You are given an ArrayList containing a sequence of elements.
+            ////try to reverse the order of elements in the ArrayList in-place(in the same arrayList)
+            ////without using the built-in Reverse. Implement a function that takes the ArrayList as input
+            ////and modifies it to have the reversed order of elements.
+
+            //int []arr =[1,2,3,4,5,6];
+            //foreach (int item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("******************After reversed*****************");
+            //Reverse<int>(ref arr);
+            //foreach (int item in arr)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
             #endregion
         }
     }
