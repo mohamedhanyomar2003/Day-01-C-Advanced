0a2889b7e4c51b99c3de7f88b4d58b092391a57e
compuremo <compuremo@DESKTOP-BIMGJ29>
2025-08-24 22:34:43 +0300

    Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            ///***
+            // * create a generic Range<T> class that represents a range of values from a minimum value to a maximum value.
+            // * The range should support basic operations such as checking if a value is within the range and determining the length of the range.
+            //Requirements:
+            //Create a generic class named Range<T> where T represents the type of values.
+            //Implement a constructor that takes the minimum and maximum values to define the range.
+            //Implement a method IsInRange(T value) that returns true if the given  value is within the range, otherwise false.
+            //Implement a method Length() that returns the length of the range (the difference between the maximum and minimum values).
+            //Note: You can assume that the type T used in the Range<T> class implements the IComparable<T> interface to allow for comparisons.
+            //*/
+
+            //Range<int> r1 = new Range<int>(1, 8);
+            //Console.WriteLine(r1.IsInRange(0));
+            //Console.WriteLine(r1.Length());
+            #endregion
         }
     }
 }
---
+++ Assignment/Range.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Range<T> where T : IComparable<T>,INumber<T>
+    {
+        public Range(T minimum, T maximum)
+        {
+            Minimum = minimum;
+            Maximum = maximum;
+        }
+
+        public T Minimum { get; set; }
+        public T Maximum { get; set; }
+
+        public bool IsInRange(T value)
+        {
+            if (value.CompareTo(Minimum) >= 0 && value.CompareTo(Maximum) <= 0) return true;
+            else return false;
+        }
+
+        public T Length()
+        {
+            return Maximum-Minimum;
+        }
+
+    }
+}
